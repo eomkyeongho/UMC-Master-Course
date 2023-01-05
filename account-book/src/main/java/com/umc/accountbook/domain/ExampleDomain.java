432db924654a74a2e5504d98bd595ffbe3ca6f12
ekh <52539130+eomkyeongho@users.noreply.github.com>
2023-01-06 06:23:10 +0900

    init

---
+++ account-book/src/main/java/com/umc/accountbook/domain/ExampleDomain.java
@@ -0,0 +1,12 @@
+package com.umc.accountbook.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+// DAO
+public class ExampleDomain {
+    private Long ex_id;
+    private String ex_name;
+}

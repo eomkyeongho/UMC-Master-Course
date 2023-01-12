cc90d3b7838441d85d49e29b6ffc1b5684912f00
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-01-12 16:11:15 +0900

    feat:test

--- account-book/src/main/java/com/umc/accountbook/controller/ExampleController.java
+++ account-book/src/main/java/com/umc/accountbook/controller/ExampleController.java
@@ -1,11 +1,14 @@
 package com.umc.accountbook.controller;
 
+import com.umc.accountbook.domain.EssentialSpending;
 import com.umc.accountbook.domain.ExampleDomain;
 import com.umc.accountbook.service.ExampleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +26,12 @@ public class ExampleController {
         ExampleDomain exDomain = testService.getExampleDomain(id);
 
         return exDomain;
+    }
+
+    @GetMapping("/testapi/{goal_id}")
+    public List<EssentialSpending> testAPI(@PathVariable int goal_id) {
+        List<EssentialSpending> esList = testService.getEssentialSpendingList(goal_id);
+
+        return esList;
     }
 }

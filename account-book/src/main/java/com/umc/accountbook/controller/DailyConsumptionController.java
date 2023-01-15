9e547543e0e7ed8f78ea2c91ff7513695174e194
sebbbin <nsb08045@daum.net>
2023-01-15 21:45:38 +0900

    feat : controller checking

--- account-book/src/main/java/com/umc/accountbook/controller/DailyConsumptionController.java
+++ account-book/src/main/java/com/umc/accountbook/controller/DailyConsumptionController.java
@@ -2,8 +2,11 @@ package com.umc.accountbook.controller;
 
 import com.umc.accountbook.service.DailyConsumptionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("daily-consumption")
 public class DailyConsumptionController {
     private final DailyConsumptionService dailyConsumptionService;
+
+    @GetMapping ("/check")
+    public ResponseEntity<String> checkTodaySuccess(@RequestParam String date){
+        return ResponseEntity.ok(date);
+    }
+
+
 }

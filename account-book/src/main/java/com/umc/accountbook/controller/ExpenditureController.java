9334c3baccd6f93e8276c8ac7a043b55b4f80e60
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-01-15 21:34:36 +0900

    modi : getDailyConsumption -> getExpenditures

--- account-book/src/main/java/com/umc/accountbook/controller/ExpenditureController.java
+++ account-book/src/main/java/com/umc/accountbook/controller/ExpenditureController.java
@@ -18,10 +18,10 @@ public class ExpenditureController {
     private final ExpenditureService expenditureService;
 
     @GetMapping("/dailyComsumption/{dc_id}")
-    public List<Expenditure> getDailyConsumptionList(@PathVariable int dc_id) {
-        List<Expenditure> dailyConsumptionList = expenditureService.getDailyConsumptionList(dc_id);
+    public List<Expenditure> getExpenditures(@PathVariable int dc_id) {
+        List<Expenditure> expenditureList = expenditureService.getExpenditures(dc_id);
 
-        return dailyConsumptionList;
+        return expenditureList;
     }
 
     @GetMapping("/example")
--- account-book/src/main/java/com/umc/accountbook/mapper/ExpenditureMapper.java
+++ account-book/src/main/java/com/umc/accountbook/mapper/ExpenditureMapper.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 @Mapper
 public interface ExpenditureMapper {
-    List<Expenditure> getDailyConsumptionList(int dc_id);
+    List<Expenditure> getExpenditures(int dc_id);
 }

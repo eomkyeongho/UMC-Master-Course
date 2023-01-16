06ba3a9166d59cd4fdb1d8dbf3896ab9bbd9381a
sebbbin <nsb08045@daum.net>
2023-01-16 12:25:14 +0900

    feat : controller, mapping, service but finish yet

--- account-book/src/main/java/com/umc/accountbook/mapper/DailyConsumptionMapper.java
+++ account-book/src/main/java/com/umc/accountbook/mapper/DailyConsumptionMapper.java
@@ -1,7 +1,14 @@
 package com.umc.accountbook.mapper;
 
+import com.umc.accountbook.domain.DailyConsumption;
+import com.umc.accountbook.domain.Expenditure;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface DailyConsumptionMapper {
+    void checkTodaySuccess (DailyConsumption dailyConsumption);
+    List<DailyConsumption> getDailyConsumption(int user_id);
+
 }

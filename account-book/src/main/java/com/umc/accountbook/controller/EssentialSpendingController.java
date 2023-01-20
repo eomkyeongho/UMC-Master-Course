9cd6d22ee26233e13c431747622cf77b36c1e47d
sebbbin <nsb08045@daum.net>
2023-01-20 21:52:26 +0900

    feat : createEssentialSpending

--- account-book/src/main/java/com/umc/accountbook/controller/EssentialSpendingController.java
+++ account-book/src/main/java/com/umc/accountbook/controller/EssentialSpendingController.java
@@ -2,13 +2,11 @@ package com.umc.accountbook.controller;
 
 import com.umc.accountbook.domain.EssentialSpending;
 import com.umc.accountbook.domain.Expenditure;
+import com.umc.accountbook.dto.CreateEssentialSpendingRequestDto;
 import com.umc.accountbook.service.EssentialSpendingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +17,18 @@ public class EssentialSpendingController {
     private final EssentialSpendingService essentialSpendingService;
 
     @GetMapping("/{goal_id}")
-    public List<EssentialSpending> getEssenailSpending(@PathVariable int goal_id) {
+    public List<EssentialSpending> getEssensialSpending(@PathVariable int goal_id) {
         List<EssentialSpending> essentialSpendingList = essentialSpendingService.getEssentialSpending(goal_id);
 
         return essentialSpendingList;
     }
+
+    @PostMapping("/create")
+    public void createEssentialSpending(@RequestBody CreateEssentialSpendingRequestDto requestDto){
+        essentialSpendingService.createEssentialSpending(requestDto.getGoal_id(), requestDto.getIcon(), requestDto.getContent(), requestDto.getCost());
+    }
+
+
+
 
 }
--- account-book/src/main/java/com/umc/accountbook/mapper/EssentialSpendingMapper.java
+++ account-book/src/main/java/com/umc/accountbook/mapper/EssentialSpendingMapper.java
@@ -1,7 +1,9 @@
 package com.umc.accountbook.mapper;
 
+import com.umc.accountbook.domain.DailyConsumption;
 import com.umc.accountbook.domain.EssentialSpending;
 import com.umc.accountbook.domain.Expenditure;
+import com.umc.accountbook.dto.CreateEssentialSpendingRequestDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -9,4 +11,5 @@ import java.util.List;
 @Mapper
 public interface EssentialSpendingMapper {
     List<EssentialSpending> getEssentialSpending(int goal_id);
+    void createEssentialSpending (int goal_id, Long icon, String content, Long cost);
 }

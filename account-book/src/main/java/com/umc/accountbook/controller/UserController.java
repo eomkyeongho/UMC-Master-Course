14cc0659401109f19a02611a63a16ba942db70d1
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2023-01-21 12:18:47 +0900

    feat : kakao access token 발급

--- account-book/src/main/java/com/umc/accountbook/controller/UserController.java
+++ account-book/src/main/java/com/umc/accountbook/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.umc.accountbook.controller;
 
+import com.umc.accountbook.service.KakaoService;
 import com.umc.accountbook.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("user")
 public class UserController {
     private final UserService userService;
+    private final KakaoService kakaoService;
+
+    @ResponseBody
+    @GetMapping("/kakao")
+    public void kakaoCallback(@RequestParam String code) {
+        kakaoService.getKakaoAccessToken(code);
+    }
 }

78064dba105740860d207c08f4fedf0a26382150
ekh <52539130+eomkyeongho@users.noreply.github.com>
2023-01-23 20:37:46 +0900

    feat : getProgressBar

--- account-book/src/main/java/com/umc/accountbook/mapper/GoalMapper.java
+++ account-book/src/main/java/com/umc/accountbook/mapper/GoalMapper.java
@@ -3,9 +3,13 @@ package com.umc.accountbook.mapper;
 import com.umc.accountbook.domain.Goal;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Map;
+
 @Mapper
 public interface GoalMapper {
     void createGoal(Goal goal);
     long getDailyAvailAmount(int goal_id, int user_id);
     long getRestAmount(int user_id, int goal_id);
+
+    Map<String, Object> getProgressBar(int user_id);
 }

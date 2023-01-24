cdd245a01634dab192728959d91fff0cee19d2ac
ekh <52539130+eomkyeongho@users.noreply.github.com>
2023-01-24 18:29:17 +0900

    feat: naver login

--- account-book/src/main/java/com/umc/accountbook/domain/User.java
+++ account-book/src/main/java/com/umc/accountbook/domain/User.java
@@ -1,13 +1,28 @@
 package com.umc.accountbook.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@Getter @Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int user_id;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private String role;
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }

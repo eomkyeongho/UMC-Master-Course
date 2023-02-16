429fa5ee2fdb0b314349de821e919fe3cb39331c
dlawltn0708 <dlawotn3@naver.com>
2023-02-16 14:41:59 +0900

    feat: essential_spending session

--- account-book/src/main/java/com/umc/accountbook/controller/EssentialSpendingController.java
+++ account-book/src/main/java/com/umc/accountbook/controller/EssentialSpendingController.java
@@ -4,12 +4,14 @@ import com.umc.accountbook.domain.EssentialSpending;
 import com.umc.accountbook.domain.Expenditure;
 import com.umc.accountbook.service.EssentialSpendingService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("essential-consumption")
@@ -20,7 +22,13 @@ public class EssentialSpendingController {
 
     @GetMapping("/essential-spending-list")
     public List<EssentialSpending> getEssentialSpending(@RequestParam int essential_spending_id, @RequestParam int goal_id) {
+        String sessionEmail = (String) httpSession.getAttribute("email");
+        if(sessionEmail == null){
+            // 리다이렉트 못하나
+            log.info("getEssential: 잘못된 접근");
+        }
         List<EssentialSpending> essentialSpendingList = essentialSpendingService.getEssentialSpending(essential_spending_id,goal_id);
+        log.info("getEssential: 성공");
         return essentialSpendingList;
     }
 
